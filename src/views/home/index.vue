8e257c48988fc51db847a35ea0be8fdb61a34a9d
黄钟飞 <huangzhongfei@sxmaps.com>
2022-01-07 19:37:18 +0800

    样式前期预备

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,15 @@
 <template>
-    <div>
-        首页
-        <van-button type="primary" size="small" round @click="push">测试跳转</van-button>
-    </div>
+    <div>首页</div>
 </template>
 
 <script>
 export default {
     methods: {
         push() {
-            this.$router.push('product')
-        }
-    }
-}
+            this.$router.push("product");
+        },
+    },
+};
 </script>
 
 <style></style>
